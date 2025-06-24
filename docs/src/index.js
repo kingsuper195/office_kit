7d0b5952bac2e17af6633638558c9277f16f58be
Peter Durdin <peter.durdin.superworld@gmail.com>
2025-06-24 19:29:11 +1000

    got basic file import export system

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,4 +1,6 @@
-const ipc = require('electron').ipcRenderer;
+const electron = require('electron');
+const ipc = electron.ipcRenderer;
+const $ = require('jquery');
 
 const quill = new Quill('#editor', {
     theme: 'snow'
@@ -9,4 +11,24 @@ function closeApp(e) {
 
 }
 
-document.getElementById("closeBtn").addEventListener("click", closeApp);
+$("#closeBtn").on("click", closeApp);
+
+$("#fileButton").on("click", () => {
+    if (document.getElementById("fileMenu").style.display == "none") {
+        document.getElementById("fileMenu").style.display = "block";
+    } else {
+        document.getElementById("fileMenu").style.display = "none";
+    }
+
+});
+
+$("#saveFileButton").on("click", () => {
+    ipc.send('saveFileDialog', quill.getSemanticHTML());
+});
+
+$("#openFileButton").on("click", async () => {
+    let data = await ipc.invoke("openFileDialog");
+    console.log(data);
+    quill.root.innerHTML=data;
+});
+
--- docs/src/main.js
+++ docs/src/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const fs = require("node:fs")
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -19,4 +20,24 @@ app.whenReady().then(() => {
 
 ipcMain.on('close', () => {
   app.quit();
+});
+
+ipcMain.on('saveFileDialog', async (event, data) => {
+  let path = await dialog.showSaveDialog({ properties: [] });
+  if (path.canceled) {
+    console.log("File save canceled");
+    return;
+  }
+  fs.writeFileSync(path.filePath, data);
+  console.log("saved");
+});
+
+ipcMain.handle('openFileDialog', async (event, data) => {
+  let path = await dialog.showOpenDialog({ properties: ['openFile'] });
+  if (path.canceled) {
+    console.log("File save canceled");
+    return;
+  }
+  let result = fs.readFileSync(path.filePaths[0],"utf-8");
+  return result;
 });
