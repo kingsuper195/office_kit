e3c6eb856cf4d6c2b6da6c82b8eb4f47dd8b7720
Peter Durdin <peter.durdin.superworld@gmail.com>
2025-06-28 07:56:08 +1000

    Init sheets

--- sheets/src/main.js
+++ sheets/src/main.js
@@ -49,53 +49,6 @@ ipcMain.on('close', () => {
   app.quit();
 });
 
-ipcMain.handle('saveFileDialog', async (event, data) => {
-  if (fed) {
-    fs.writeFileSync(fed, data);
-    return fed;
-  }
-  let path = await dialog.showSaveDialog({ defaultPath: "New File.htmls", filters: [{ name: 'HTML Snippet', extensions: ['htmls'] }] });
-  if (path.canceled) {
-    console.log("File save canceled");
-    return;
-  }
-  if (path.filePath.endsWith('.htmls')) {
-    fed = path.filePath
-  } else {
-    fed = path.filePath + ".htmls"
-  }
-  fs.writeFileSync(fed, data);
-  console.log("saved");
-  return fed;
-});
-
-ipcMain.handle('saveFileAsDialog', async (event, data) => {
-  let path = await dialog.showSaveDialog({ filters: [{ name: 'HTML Snippet', extensions: ['htmls'] }] });
-  if (path.canceled) {
-    console.log("File save canceled");
-    return fed;
-  }
-  if (path.filePath.endsWith('.htmls')) {
-    fed = path.filePath
-  } else {
-    fed = path.filePath + ".htmls"
-  }
-  fs.writeFileSync(fed, data);
-  console.log("saved");
-  return fed;
-});
-
-ipcMain.handle('openFileDialog', async (event, data) => {
-  let path = await dialog.showOpenDialog({ filters: [{ name: 'HTML Snippet', extensions: ['htmls'] }], properties: ['openFile'] });
-  if (path.canceled) {
-    console.log("File open canceled");
-    return;
-  }
-  fed = path.filePaths[0];
-  let result = fs.readFileSync(path.filePaths[0], "utf-8");
-  return { data: result, fed };
-});
-
 ipcMain.on('minimize', () => {
   win.minimize()
 });
